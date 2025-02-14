2977533993dfd206d2747ca0774b70a7cf18624f
lrizza <leonardorizza@hotmail.com>
2025-02-14 09:40:10 -0300

    primera

---
+++ main.js
@@ -0,0 +1,9 @@
+document.querySelectorAll('.leo-card').forEach(card => {
+    card.addEventListener('mouseover', () => {
+        card.querySelector('.leo-service-btn').style.transition = 'bottom 0.3s ease-in-out, transform 0.3s ease-in-out, background-color 0.3s';
+    });
+
+    card.addEventListener('mouseleave', () => {
+        card.querySelector('.leo-service-btn').style.transition = 'bottom 0.3s ease-in-out, transform 0.3s ease-in-out, background-color 0.3s';
+    });
+});
